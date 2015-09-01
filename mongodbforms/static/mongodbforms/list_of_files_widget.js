864cfc18806911a7907dca2d1b8faec2c606b7d5
m-vdb <mvergerdelbove@work4labs.com>
2015-09-01 14:18:40 -0700

    JKR-9392: replace the name of the inputs too

--- mongodbforms/static/mongodbforms/list_of_files_widget.js
+++ mongodbforms/static/mongodbforms/list_of_files_widget.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var itemTemplate,
+        lastItemId, newItemId,
         count = $(".js-widget-item").length,
         fieldNameMatch = $(".js-widget-last-item input:first").attr("id").match(/id_([a-z_]+)_\d+_\d+/),
         fieldName = fieldNameMatch ? fieldNameMatch[1] : null;
@@ -8,9 +9,13 @@ $(document).ready(function() {
         throw new Error("Missing field name.");
     }
 
+    lastItemId = fieldName + "_" + (count - 1) + "_";
+    newItemId = fieldName + "_{count}_";
     itemTemplate = $(".js-widget-last-item").prop("outerHTML");
-    itemTemplate = itemTemplate.replace("id_" + fieldName + "_" + (count - 1) + "_0", "id_" + fieldName + "_{count}_0");
-    itemTemplate = itemTemplate.replace("id_" + fieldName + "_" + (count - 1) + "_1", "id_" + fieldName + "_{count}_1");
+    itemTemplate = itemTemplate.replace("id_" + lastItemId + "0", "id_" + newItemId + "0");
+    itemTemplate = itemTemplate.replace("id_" + lastItemId + "1", "id_" + newItemId + "1");
+    itemTemplate = itemTemplate.replace(lastItemId + "0", newItemId + "0");
+    itemTemplate = itemTemplate.replace(lastItemId + "1", newItemId + "1");
 
     var $lastItem = $(".js-widget-last-item");
 
