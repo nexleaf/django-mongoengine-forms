796fe6786f8f05349c5d3be62ebfee663730ad3c
m-vdb <mvergerdelbove@work4labs.com>
2016-09-19 17:03:00 -0700

    bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def convert_readme():
 
 setup(
     name='django-mongoengine-forms',
-    version='0.4.1.6',
+    version='0.4.1.7',
     description="An implementation of django forms using mongoengine.",
     author='Thom Wiggers',
     author_email='thom@thomwiggers.nl',
